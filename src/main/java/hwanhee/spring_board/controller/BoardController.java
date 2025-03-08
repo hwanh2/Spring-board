93f306fa8fa7def6c01cccb8704afab2da5b2fc1
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-03-08 16:14:39 +0900

    index page

--- src/main/java/hwanhee/spring_board/controller/BoardController.java
+++ src/main/java/hwanhee/spring_board/controller/BoardController.java
@@ -3,11 +3,13 @@ package hwanhee.spring_board.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BaseController {
-    @GetMapping("/")
-    public String index(){
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("")
+    public String list(){
         return "index";
     }
 }
