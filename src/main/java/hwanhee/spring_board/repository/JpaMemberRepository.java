ff27a6f776b9a36ca922bfae84851f687b0f4b14
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-03-08 23:28:51 +0900

    jpa로 repositoey 구현

---
+++ src/main/java/hwanhee/spring_board/repository/JpaMemberRepository.java
@@ -0,0 +1,45 @@
+package hwanhee.spring_board.repository;
+
+import hwanhee.spring_board.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class JpaMemberRepository implements MemberRepository {
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+    private final EntityManager em;
+    @Override
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class,id);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        List<Member> result = em.createQuery("select m from Member m where m.name= :name",Member.class)
+                .setParameter("name",name)
+                .getResultList();
+
+        return result.stream().findAny();
+    }
+
+    @Override
+    public List<Member> findByAll() {
+        return em.createQuery("select m from Member m",Member.class)
+                .getResultList();
+    }
+}
