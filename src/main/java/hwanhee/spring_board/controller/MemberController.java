b984b04ce08b00c72b13227dc407a48dd46383de
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-03-09 18:14:25 +0900

    컨트롤러에서 회원가입 구현

--- src/main/java/hwanhee/spring_board/controller/MemberController.java
+++ src/main/java/hwanhee/spring_board/controller/MemberController.java
@@ -1,6 +1,8 @@
 package hwanhee.spring_board.controller;
 
+import hwanhee.spring_board.domain.Member;
 import hwanhee.spring_board.service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,16 @@ public class MemberController {
     @GetMapping("/signup")
     public String signupForm(){
         return "members/signup";
+    }
+
+    @PostMapping("/signup")
+    public String signup(HttpSession session,MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setPassword(form.getPassword());
+        memberService.join(member);
+        session.setAttribute("name", member.getName());
+        return "redirect:/";
     }
 
 }
