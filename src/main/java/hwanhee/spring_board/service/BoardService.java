5f4e004320c44a5bb03b9f2c1b961108646a443f
hwanhee <hwanhee@hwanheeui-MacBookPro.local>
2025-03-10 14:16:32 +0900

    BoardService 구현

--- src/main/java/hwanhee/spring_board/service/BoardService.java
+++ src/main/java/hwanhee/spring_board/service/BoardService.java
@@ -1,4 +1,35 @@
 package hwanhee.spring_board.service;
 
+import hwanhee.spring_board.domain.Board;
+import hwanhee.spring_board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BoardService {
+    private final BoardRepository boardRepository;
+
+    @Autowired
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public Board saveBoard(Board board){
+        boardRepository.save(board);
+        return board;
+    }
+
+    public List<Board> findBoards(){
+        return boardRepository.findAll();
+    }
+
+    public List<Board> findByWriterId(Long id){
+        return boardRepository.findByWriterId(id);
+    }
+
+    public void deleteBoardById(Long id) {
+        boardRepository.deleteById(id);
+    }
 }
